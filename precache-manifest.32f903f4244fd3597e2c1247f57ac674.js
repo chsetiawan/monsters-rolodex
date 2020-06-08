ab15f12fedae20ee42f3883c471fa8a235cb5c89
Christian <>
2020-06-08 00:28:40 -0700

    Updates

---
+++ precache-manifest.32f903f4244fd3597e2c1247f57ac674.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7dc46ad15d5b0da566717b474ee57fc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1b6e9792bb100dbbd06e",
+    "url": "/monsters-rolodex/static/css/main.b3f39e83.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b6e9792bb100dbbd06e",
+    "url": "/monsters-rolodex/static/js/main.afad61f2.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
